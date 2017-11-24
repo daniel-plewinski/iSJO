fcd8d417df87147cd46efdaadf0251177f36f415
Daniel Plewinski <dplewinski@gmail.com>
2017-11-24 20:19:16 +0100

    User class completed and school class started

---
+++ src/AppBundle/Form/AddTeacherType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddTeacherType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'email',
+                TextType::class,
+                [
+                    'label' => "e-mail",
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                ]
+            )
+            ->add(
+                'username',
+                TextType::class,
+                [
+                    'label' => "Nazwa użytkownika",
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                ]
+
+            ) ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => "Imię i nazwisko",
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                ]
+            )
+            ->add(
+                "submit",
+                SubmitType::class,
+                [
+                    "label" => "Dodaj",
+
+                    'attr' => [
+                        'class' => 'top-marg btn btn-primary',
+                    ],
+                ]
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'AppBundle\Entity\User',
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user';
+    }
+
+
+}
